0ad8c24a2f0848cc21b75be838085997022dfc9d
Christopher Pedersen <chrisaped@gmail.com>
2017-11-25 11:34:57 -0500

    product indent fix

--- lib/product.rb
+++ lib/product.rb
@@ -1,22 +1,22 @@
 class Product
-	attr_accessor :name, :price, :quantity
+  attr_accessor :name, :price, :quantity
 
-	def initialize(name, price)
-		@name = name
-		@price = price
-		@quantity = 2
-	end
+  def initialize(name, price)
+	  @name = name
+	  @price = price
+	  @quantity = 2
+  end
 
-	def increase_quantity
+  def increase_quantity
     puts "Please enter the new quantity of this product."
     quantity = gets.chomp.to_i
     if quantity > 0
-    	@quantity += quantity
-    	puts "Your product's quantity has been updated!"
-    	puts "#{@name}, Quantity: #{@quantity}"
+      @quantity += quantity
+      puts "Your product's quantity has been updated!"
+      puts "#{@name}, Quantity: #{@quantity}"
     else
-    	puts "Incorrect quantity. Please try again."
-    	increase_quantity
+      puts "Incorrect quantity. Please try again."
+      increase_quantity
     end		
-	end	
+  end	
 end
